0998b05ce7da1b694a3c9e92b22edd6f45d1d04a
anton0380 <aa@unelma.ru>
2021-07-21 16:22:04 +0300

    create counter in react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore, bindActionCreators } from "redux";
 import reducer from './reducer';
 import * as actions from './actions';
+import Counter from './counter';
 
 const store = createStore(reducer);
 const {dispatch} = store;
@@ -8,17 +11,26 @@ const {dispatch} = store;
 const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 // const decDispatch = bindActionCreators(dec, dispatch);
 // const rndDispatch = bindActionCreators(rnd, dispatch);
+ 
+// document.getElementById('inc').addEventListener('click', inc);
+// document.getElementById('dec').addEventListener('click', dec);
 
-document.getElementById('inc').addEventListener('click', inc);
-document.getElementById('dec').addEventListener('click', dec);
-
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-});
+// document.getElementById('rnd').addEventListener('click', () => {
+//   const value = Math.floor(Math.random() * 10);
+//   rnd(value);
+// });
 
 const update = () => {
-  document.getElementById('counter').textContent = store.getState();
+  ReactDOM.render(<Counter
+    counter={store.getState()}
+    inc={inc}
+    dec={dec}  
+    rnd={() => {
+      const value = Math.floor(Math.random() * 10);
+      rnd(value);
+    }}/>, document.getElementById('root'));
 }
 
+update();
 store.subscribe(update);
+
