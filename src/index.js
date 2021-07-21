c56186b690b4bfeec2dcb8fa2030add069a920e6
anton0380 <aa@unelma.ru>
2021-07-21 15:02:37 +0300

    3 buttons and result

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('Hello redux');
+import { createStore } from "redux";
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    case 'DEC':
+      return state - 1;
+    case 'RND':
+      return state + action.value;
+        default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch({type: 'INC'})
+});
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch({type: 'DEC'})
+});
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10);
+  store.dispatch({type: 'RND', value})
+});
+
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
